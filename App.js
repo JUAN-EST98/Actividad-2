c36728e2fae0e58a09790e19a5c69595546d3e2f
JUAN-EST98 <juanest.an311098@gmail.com>
2025-03-06 18:28:57 -0500

    subo pryoecto
    archivos flebox rn

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header} />
+
+      <View style={styles.row}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        
+      </View>
+
+      <View style={styles.middle} />
+
+      <View style={styles.grid}>
+        <View style={styles.smallBox} />
+        <View style={styles.smallBox} />
+        <View style={styles.smallBox} />
+        <View style={styles.smallBox} />
+        <View style={styles.smallBox} />
+        <View style={styles.smallBox} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 20,
+  },
+  header: {
+    width: 330, 
+    height: 100, 
+    backgroundColor: 'green',
+    borderRadius: 15,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 330,
+    marginVertical: 10, 
+  },
+  box: {
+    flex: 1,  
+    height: 120, 
+    backgroundColor: 'black',
+    marginHorizontal: 5, 
+    borderRadius: 15,
+  },
+  middle: {
+    width: 330,
+    height: 60,  
+    backgroundColor: 'maroon',
+    borderRadius: 15,
+    marginVertical: 10,
+  },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    width: 330,
+  },
+  smallBox: {
+    width: 100,  
+    height: 80,  
+    backgroundColor: 'blue',
+    marginBottom: 10,
+    borderRadius: 15,
   },
 });
+
